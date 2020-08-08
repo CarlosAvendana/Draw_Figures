af1d340d0be701b05be082597458d2bd22971422
Carlos Obando Avendaña <carlos.obando.avendana@est.una.ac.cr>
2020-08-08 15:02:46 -0600

    Primer commit
    Base del proyecto

---
+++ Draw_Figures_V1/src/model/Figure.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Figure {
+
+    protected int indentifier;
+
+    @Override
+    public String toString() {
+        return "Figure{" + "indentifier=" + indentifier + '}';
+    }
+
+}
