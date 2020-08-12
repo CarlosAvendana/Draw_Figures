87def4bcef9afd8a11d66058c7a8809916c0f9cb
Carlos Obando Avendaña <carlos.obando.avendana@est.una.ac.cr>
2020-08-11 18:30:56 -0600

    Cambios

--- Draw_Figures_V1/src/view/Ventana.java
+++ Draw_Figures_V1/src/view/Ventana.java
@@ -1,16 +1,12 @@
-
 package view;
-
 
 public class Ventana extends javax.swing.JFrame {
 
-    
     public Ventana() {
         initComponents();
         setResizable(false);
     }
 
-    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
